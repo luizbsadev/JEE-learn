6c00a9c5abad8a9572f09b5245622432b323b5d1
luizbsadev <luizbsadev@gmail.com>
2025-06-08 22:43:21 -0300

    Hello world

---
+++ bank/src/main/java/com/bank/servlet/homeServlet.java
@@ -0,0 +1,19 @@
+package com.bank.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/home")
+public class homeServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/plain");
+        resp.getWriter().write("olá, Jboss");
+    }
+}
